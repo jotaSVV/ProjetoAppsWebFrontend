391b989e7188b22fa0b1d0fc3e94f2edea013eb7
jotaSVV <jsvtop@gmail.com>
2022-04-12 23:09:12 +0100

    first commit

---
+++ app/adapters/application.js
@@ -0,0 +1,17 @@
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
+import { inject as service } from '@ember/service';
+export default class ApplicationAdapter extends JSONAPIAdapter {
+
+@service session;
+
+
+@computed('session.data.authenticated.token')
+get headers() {
+    let headers = {};
+    if (this.session.isAuthenticated) {
+      headers['token'] = `Token ${this.session.data.authenticated.token}`;
+    }
+    return headers;
+}
+
+}
---
+++ app/routes/register.js
@@ -0,0 +1,11 @@
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+export default class RegisterRoute extends Route {
+
+
+    @service session;
+
+  beforeModel() {
+    this.session.prohibitAuthentication('dashboard');
+  }
+}
