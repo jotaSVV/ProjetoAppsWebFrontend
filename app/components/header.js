cfe1cb6acd57d08070a4828f0bdca27fc0d1287b
ppinheiro99 <ppinheiro99@hotmail.com>
2022-04-20 23:09:25 +0100

    design do frontend smi-feito

--- app/components/header.js
+++ app/components/header.js
@@ -1,30 +1,42 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Ember from 'ember';
 
-export default class HeaderComponent extends Component {
-  @service session;
+export default Ember.Component.extend({
+    /**@service session;*/
 
-  @action
-  logout() {
-    // RouterService.transitionTo('login');
-     this.session.invalidate();
-    // let response = await fetch('http://localhost:8081/api/v1/auth/logout', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Access-Control-Allow-Origin': '*',
-    //         'Access-Control-Allow-Methods': 'HEAD, GET, POST, PUT, PATCH, DELETE',
-    //         'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
-    //         'Content-Type': 'application/json',
-    //       },
-    // });
-    // if (response.ok) {
-    //     this.session.invalidate();
-    //     this.transitionToRoute('login');
-    //     return await response.json();
-    // } else {
-    //     let error = await response.json();
-    //     throw new Error(error.message);
-    // }
-  }
-}
+
+    isShowingFilterModal: false,
+    isShowingSearchModal: false,
+
+    actions: {
+        openFilterModal() {
+            this.set('isShowingFilterModal', !this.isShowingFilterModal);
+        },
+        openSearchModal() {
+            this.set('isShowingSearchModal', !this.isShowingSearchModal);
+        },
+        logout() {
+            // RouterService.transitionTo('login');
+            this.session.invalidate();
+            // let response = await fetch('http://localhost:8081/api/v1/auth/logout', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Access-Control-Allow-Origin': '*',
+            //         'Access-Control-Allow-Methods': 'HEAD, GET, POST, PUT, PATCH, DELETE',
+            //         'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+            //         'Content-Type': 'application/json',
+            //       },
+            // });
+            // if (response.ok) {
+            //     this.session.invalidate();
+            //     this.transitionToRoute('login');
+            //     return await response.json();
+            // } else {
+            //     let error = await response.json();
+            //     throw new Error(error.message);
+            // }
+        }
+    }
+})
--- app/components/map.js
+++ app/components/map.js
@@ -1,6 +1,10 @@
 import Component from '@glimmer/component';
-export default class MapComponent extends Component {
-    lat = 41.1579438;
-    lng = -8.6291053;
-    zoom = 16;
-}
+import { action } from '@ember/object';
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    lat: 41.1579438,
+    lng: -8.6291053,
+    zoom: 16,
+
+})
