68214bee4c5baff5b24903daa5164488e552f053
ppinheiro99 <ppinheiro99@hotmail.com>
2022-04-21 23:36:47 +0100

    popups

--- app/components/header.js
+++ app/components/header.js
@@ -4,11 +4,12 @@ import { inject as service } from '@ember/service';
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    /**@service session;*/
+    /**@service session;**/
 
 
     isShowingFilterModal: false,
     isShowingSearchModal: false,
+    active: false,
 
     actions: {
         openFilterModal() {
@@ -37,6 +38,33 @@ export default Ember.Component.extend({
             //     let error = await response.json();
             //     throw new Error(error.message);
             // }
+        },
+        openSelect(){
+            this.toggleProperty('active')
+            console.warn(this.get('#modal-overlays .modal-filter .dropdown .dropdown-menu'))
+            console.warn(Ember.$('#modal-overlays .modal-filter .dropdown .dropdown-menu').find('.dropdown-menu'))
         }
+
     }
 })
+
+/* Dropdown Menu
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+
+$('.dropdown-menu li').click(function () {
+  var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
+  msg = '<span class="msg">Hidden input value: ';
+  $('.msg').html(msg + input + '</span>');
+});*/
